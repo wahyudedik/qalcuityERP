4a5ff823828301e51e0c84a070e91192e7a9daac
wahyudedik <wdedyk@gmail.com>
2026-03-21 23:11:53 +0700

    DDD

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use Database\Factories\UserFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
